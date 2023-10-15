b620e82fee1d6ce57c38820be323c00768b20075
rudro54 <rudro54@gmail.com>
2023-10-15 14:33:05 +0600

    taskOneCompleted

---
+++ personal_info.php
@@ -0,0 +1,16 @@
+<?php
+
+// Task 1: Personal Information Page
+// Create a PHP file named personal_info.php that displays your personal information using variables and the echo statement. Include your name, age, country, and a brief introduction.
+
+
+$name = "Abdullah Al Zayed";
+$age = 44;
+$country = "Bangladesh";
+$briefIntroduction = "Hey. I am Zayed. Last I worked, as head of operations in a private conglomerate. I did my BSC in CSE 22 years back and MBA afterwards. I have a strong experience background in operations management , export , import , international business and supply chain. I am now trying to explore the software development field. Wish me luck!! ";
+
+
+echo "Name : " . $name . "\n";
+echo "Age : " . $age . "\n";
+echo "Country : " . $country . "\n";
+echo "Brief Intro : " . $briefIntroduction . "\n";
